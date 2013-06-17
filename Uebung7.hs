8c375a327fe12c296a42061af24d0f08e7422b18
Julian Wefers <julian.wefers@gmail.com>
2013-06-17 15:58:41 +0200

    reformatting

--- Uebung7.hs
+++ Uebung7.hs
@@ -1,54 +1,43 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
 
--- Uebung 6 Funktionale Programmierung
+-- Uebung 7 Funktionale Programmierung
 -- Von Julian Wefers und Alexander Mills
- 
--- The following compiler options are neccessary
--- for the extension concerning the error handling
--- they are not required for this simple example
- 
 -- ----------------------------------------
---
--- Monadic version of simple expression
--- evaluator.
--- No extensions, same semantics as in Expr0
---
--- ----------------------------------------
- 
-module Expr1 where
- 
+
+module Expr7 where
+
 import Data.Maybe ( fromMaybe )
- 
+
 import Control.Monad ( liftM2 )
 import Control.Monad.Error ( MonadError ( .. ) )
 import Control.Monad.Reader ( MonadReader ( .. ) )
 
 -- ----------------------------------------
 -- syntactic domains
- 
+
 data Expr  = Const  Int
            | Var    Id                -- NEU
            | Let    Id    Expr Expr   -- NEU
            | Binary BinOp Expr Expr
              deriving (Show)
- 
+
 data BinOp = Add | Sub | Mul | Div | Mod
              deriving (Eq, Show)
- 
+
 type Id    = String
- 
+
 -- ----------------------------------------
 -- semantic domains
 
 newtype Result a
            = Res { unRes :: Env -> ResVal a }
- 
+
 data ResVal a
            = Val { val :: a }
            | Exc { exc :: String }
              deriving (Show)
- 
+
 type Env   = [(Id, Int)]
 
 instance Monad ResVal where
@@ -67,7 +56,7 @@ instance Monad Result where
 
 instance MonadError String Result where
   -- wrap an exception string and ignore any environment
-  throwError  = Res . const . Exc
+  throwError    = Res . const . Exc
 
   -- If f throws an Error, the handler is applied, otherwise ignored
   catchError (Res f) handler
@@ -75,7 +64,7 @@ instance MonadError String Result where
                                    (Exc e) -> unRes (handler e) env
                                    (Val v) -> Val v
 
- 
+
 instance MonadReader Env Result where
   -- take the incoming environment and wrap it in ResVal
   ask       = Res return
@@ -83,7 +72,7 @@ instance MonadReader Env Result where
   -- modify the incoming environment with f and apply it to c
   local f c = Res (unRes c . f)
 
-  -- do something with the environment and wrap the result in Result
+  -- do something with the environment and wrap that in Result
   reader f  = Res (return . f)
 
 
@@ -92,7 +81,7 @@ instance MonadReader Env Result where
 
 eval :: Expr -> Result Int
 
-eval (Const i) 
+eval (Const i)
   = return i
 
 -- try to retrieve a value for id out of the current env
@@ -104,23 +93,23 @@ eval (Var  id)
 
 -- evaluate e1 and store its value in the env for e2
 eval (Let id e1 e2)
-  = eval e1 >>= \ val -> local ((id,val):) (eval e2) 
+  = eval e1 >>= \ val -> local ((id,val):) (eval e2)
 
 eval (Binary op l r)
   = lookupMft op >>= \ mf -> mf (eval l) (eval r)
 
 -- ----------------------------------------
 -- the meaning of binary operators
- 
+
 type MF = Result Int -> Result Int -> Result Int
- 
+
 lookupMft :: BinOp -> Result MF
 lookupMft op
   = case lookup op mft of
     Nothing -> throwError
                "operation not implemented"
     Just mf -> return mf
-    
+
 mft :: [(BinOp, MF)]
 mft
   = [ (Add, liftM2 (+))
@@ -139,19 +128,19 @@ divM :: MF
 divM ma mb = do
   x <- ma
   y <- mb
-  if y == 0 
+  if   y == 0
   then throwError "division by zero"
   else return (x `div` y)
- 
+
 -- ----------------------------------------
 -- expression evaluator with outer environment
 
 evalEnv :: Expr -> Env -> ResVal Int
-evalEnv = unRes . eval
- 
+evalEnv  = unRes . eval
+
 -- ----------------------------------------
 -- sample expressions
- 
+
 e1 = Binary Mul (Binary Add (Const 4)
                        (Const 2))
                        (Const 7)
@@ -160,5 +149,5 @@ e4 = Binary Div (Const 10) (Const 0)
 e5 = Binary Div (Const 10) (Const 2)
 e6 = Let "a" (Const 5) (Var "a")
 e7 = Let "a" (Let "b" (Const 1) (Var "b")) (Let "b" (Const 2) (Binary Add (Var "a") (Var "b")))
- 
+
 -- ----------------------------------------
